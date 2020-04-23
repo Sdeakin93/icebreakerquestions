db62ccf3d81d70dcf05f947341cb3371bbc4ad55
Sarah Deakin <sarah.deakin@starlingbank.com>
2020-04-23 09:41:53 +0100

    new questions

--- src/components/Questions/questions.js
+++ src/components/Questions/questions.js
@@ -10,6 +10,15 @@ const posts = [
     },
     {
         "body" : "Demi Lovato and Sam Smith just came out with the video for their new song featuring a sports theme - what sport do you think you have the best chance of representing your country at?"
+    },
+    {
+        "body" : "Because of the lockdown, many of us have been binge watching shows on streaming services. If you had to be trapped in the universe of any notable Netflix show, which one would it be?"
+    },
+    {
+        "body" : "When promoting her 2017 album witness, Katy Perry did a four day livestream on Youtube. How long do you think you would last livestreaming your entire life?"
+    },
+    {
+        "body" : "We're all aware of the importance of looking after our health at the moment. Grey's Anatomy is a long running series about healthcare that is still going. Which TV series are you amazed ran as long as it did?"
     }
 ];
 
